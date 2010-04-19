61c43ba8b96727246927d0d2dea4e5200959343e
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-04-19 13:19:15 +0200

    Updated io lib.

--- src/io.cc
+++ src/io.cc
@@ -1,22 +1,21 @@
 /*
  io.cc -- input/output routines
- Copyright (C) 2008-2009 Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl)
+ Copyright (C) 2008--2010 Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
+ Copyright (C) 2009 Michiel van Noort
  
- This file is part of FOAM.
- 
- FOAM is free software: you can redistribute it and/or modify
+ This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
+ the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
  
- FOAM is distributed in the hope that it will be useful,
+ This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
  
  You should have received a copy of the GNU General Public License
- along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
-
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 /*! 
  @file io.cc 
@@ -28,73 +27,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <sys/socket.h>
 #include <string.h>
+#include <algorithm>
+#include <fstream>
 
 #include "io.h"
 
-Io::Io(void)
-{
-  Io(2);
+void Io::init(int l) {
+	setVerb(l);
+	logfd = NULL;
+	termfd = stdout;
 }
 
-Io::Io(int l)
-{
-  level_mask=~(0xFFFFFFFF<<l);
-  f=stderr;
-  swap_endian=0;
+Io::~Io(void) {
+	if (logfd && logfd != stdout && logfd != stderr)
+		fclose(logfd);
 }
 
-
-Io::~Io(void)
-{
-  if((f)&&(f!=stdout)&&(f!=stderr)) fclose(f);
+int Io::setLogfile(std::string file) {
+	logfile = file;
+	logfd = fopen(logfile.c_str(), "a");
+	msg(IO_DEB1, "Using logfile %s.", logfile.c_str());
+	if (!logfd) return -1;
+	return 0;
 }
 
-void Io::reconf(int level)
-{
-  level_mask=~(0xFFFFFFFF<<(verb=max(level,0)));
-}
+int Io::msg(int type, const char *fmtstr, ...) {
+	int level = type & IO_LEVEL_MASK;
+	
+	static const char *prefix[] = {"",  "err ", "warn", "info", "xnfo", "dbg1", "dbg2"};
 
-int Io::msg(int type,const char *formatstring,...)
-{
-  const char *message[]={"","error","warn","info","xinfo","debug1","debug2","",""};
-  int level=type&level_mask;
-  //int level=0;
-  //while(msg_type>>level) ++level;
-  if(level){
-    va_list ap;
-    va_start(ap,formatstring);
-    char *newformatstring;
-    if(!(type&IO_NOID)){
-      newformatstring=new char [strlen(formatstring)+strlen(message[level])+5];
-      sprintf(newformatstring,"[%s] %s\n",message[level],formatstring);
-    }else newformatstring=strcpy(new char [strlen(formatstring)+1],formatstring);
-    if(f){
-      vfprintf(f,newformatstring,ap);
-      fflush(f);
-    }else{ // network store...
-      char *str=new char [4096];    // fixed length: not so nice...
-      vsprintf(str,newformatstring,ap);
-      int size=strlen(str)+1+sizeof(int);
-      unsigned char *buf=new unsigned char [size];
-//      offs+=pack(buf+offs,type,swap_endian);
-//      offs+=pack(buf+offs,str);
-      delete[] str;
-//      if(ssock){
-//        ssock->send_cmd(iocmd);
-//        ssock->send(buf,size);
-//      }else{
-//        ::send(sock,&iocmd,1,0);
-//        ::send(sock,&size,sizeof(size),0);
-//        ::send(sock,buf,size,0);
-//      }
-      delete[] buf;
-    }
-    delete[] newformatstring;
-    va_end(ap);
-    if(type&IO_FATAL) exit(1);
-    if(type&IO_ERR) return -1;
-  }
-  return 0;
+	if (level <= verb) {
+		va_list ap;
+		va_start(ap, fmtstr);
+		char *newfmt;
+
+		if (type & IO_NOID) {
+			newfmt = strcpy(new char[strlen(fmtstr) + 1], fmtstr);
+		}
+		else if (type & IO_NOLF) {
+			newfmt = new char[strlen(fmtstr) + strlen(prefix[level]) + 4];
+			sprintf(newfmt, "[%s] %s", prefix[level], fmtstr);
+		}
+		else {
+			newfmt = new char[strlen(fmtstr) + strlen(prefix[level]) + 5];
+			sprintf(newfmt, "[%s] %s\n", prefix[level], fmtstr);			
+		}
+		char *msg;
+		vasprintf(&msg, newfmt, ap);
+		
+		fprintf(termfd, msg);
+		fflush(termfd);
+		
+		if (logfd) {
+			fprintf(logfd, msg);
+			fflush(logfd);
+		}
+		
+		free(msg);
+		delete[] newfmt;
+		va_end(ap);
+	}
+	
+	if (type & IO_FATAL) exit(-1);
+	if (type & IO_ERR) return -1;
+
+	return 0;
 }
--- src/io.h
+++ src/io.h
@@ -1,35 +1,71 @@
+/*
+ io.h -- input/output routines
+ Copyright (C) 2008--2010 Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
+ Copyright (C) 2009 Michiel van Noort
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef __IO_H__
 #define __IO_H__
 
+#include <string>
 #include <stdio.h>
 #include <unistd.h>
 
+// Logging flags
+#define IO_NOID         0x00000100      //!< Do not add loglevel string
+#define IO_FATAL        0x00000200      //!< Fatal, quit immediately
+#define IO_RETURN       0x00000400      //!< Give a non-zero return code
+#define IO_NOLF         0x00000800      //!< Do not add linefeed
+
 // Logging levels
-#define IO_ERR          0x00000001 | 0x00000400
+#define IO_ERR          0x00000001 | IO_RETURN
 #define IO_WARN         0x00000002
 #define IO_INFO         0x00000003
 #define IO_XNFO         0x00000004
 #define IO_DEB1         0x00000005
 #define IO_DEB2         0x00000006
 #define IO_LEVEL_MASK   0x000000FF
+#define IO_MAXLEVEL     0x00000006
 
-// Logging flags
-#define IO_NOID         0x00000100      //!< Do not add loglevel string
-#define IO_FATAL        0x00000200      //!< Fatal, quit immediately
-
-#define max(x,y) (x) > (y) ? (x) : (y)
+using namespace std;
 
 class Io {
-  FILE *f;
-  unsigned char iocmd,swap_endian;
-  int verb,level_mask;
+	int verb, level_mask;
+	FILE *termfd;
+	FILE *logfd;
+	string logfile;
+	
 public:
-  Io(void);
-  Io::Io(int l);
-  ~Io(void);
-  void reconf(int);
-  int msg(int,const char*,...);
-  int vlevel(void){ return verb; }
+	Io() { Io::init(IO_MAXLEVEL); }
+	Io(int l) { Io::init(l); }
+	~Io();
+	
+	void init(int);
+
+	int msg(int, const char*, ...);
+	
+	int setLogfile(string);
+	
+	int getVerb() { return verb; }
+	int setVerb(int l) { verb = max(1, min(l, IO_MAXLEVEL)); return verb; }
+	int setVerb(string l) { return setVerb((int) strtoll(l.c_str(), NULL, 0)); }
+	
+	int incVerb() { return setVerb(verb+1); }
+	int decVerb() { return setVerb(verb-1); }
 };
 
 #endif
